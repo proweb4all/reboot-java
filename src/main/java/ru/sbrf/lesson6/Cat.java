6baefb3fdd693b8e8e2bd80a35dfd32bd81fa433
proweb4all <proweb4all@narod.ru>
2020-08-18 22:32:22 +0300

    update lesson6

--- src/main/java/ru/sbrf/lesson6/Cat.java
+++ src/main/java/ru/sbrf/lesson6/Cat.java
@@ -1,9 +1,10 @@
 package ru.sbrf.lesson6;
 
 public class Cat extends Animal {
-    protected void description(String name) {
-        System.out.print("Кошка " + name + ' ' + say("мяу") + ", ");
-        System.out.print(climb("ловко") + ", но " + swim("плохо") + ", ");
-        System.out.println(eat("мышей") + " и " + move("по-кошачьи"));
+    protected String name = "";
+
+    protected String description() {
+        return "Кошка " + name + ' ' + say("мяу") + ", " + climb("ловко") + ", но " +
+                swim("плохо") + ", " + eat("мышей") + " и " + move("по-кошачьи");
     }
 }
--- src/main/java/ru/sbrf/lesson6/Main.java
+++ src/main/java/ru/sbrf/lesson6/Main.java
@@ -3,7 +3,8 @@ package ru.sbrf.lesson6;
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
-        cat.description("Мурка");
+        cat.name = "Мурка";
+        System.out.println(cat.description());
         Dog dog = new Dog();
         dog.description("Жучка");
     }
