76e4c9d631a4de64f451e64779f6303a4f412e25
proweb4all <proweb4all@narod.ru>
2020-08-22 23:38:25 +0300

    update-refactoring

--- src/main/java/ru/sbrf/lesson6/Animal.java
+++ src/main/java/ru/sbrf/lesson6/Animal.java
@@ -1,20 +1,46 @@
 package ru.sbrf.lesson6;
 
 public class Animal {
+    private String saySound = "(?)";
+    private String swimHow = "(?)";
+    private String eatWhat = "(?)";
+    private String moveHow = "(?)";
+    private String climbHow = "(?)";
+
+    protected String say() {
+        return "говорит \"" + saySound + '-' + saySound + "\"";
+    }
+
     protected String say(String sound) {
         return "говорит \"" + sound + '-' + sound + "\"";
+    }
+
+    protected String swim() {
+        return "плавает " + swimHow;
     }
 
     protected String swim(String how) {
         return "плавает " + how;
     }
 
+    protected String eat() {
+        return "кушает " + eatWhat;
+    }
+
     protected String eat(String what) {
         return "кушает " + what;
     }
 
+    protected String move() {
+        return "двигается " + moveHow;
+    }
+
     protected String move(String how) {
         return "двигается " + how;
+    }
+
+    protected String climb() {
+        return "лазает по деревьям " + climbHow;
     }
 
     protected String climb(String how) {
--- src/main/java/ru/sbrf/lesson6/Cat.java
+++ src/main/java/ru/sbrf/lesson6/Cat.java
@@ -1,10 +1,48 @@
 package ru.sbrf.lesson6;
+// Наследование
 
 public class Cat extends Animal {
-    protected String name = "";
+    private String name = "NoName";
+    private String saySound = "мяу";
+    private String swimHow = "плохо";
+    private String eatWhat = "мышей";
+    private String moveHow = "по-кошачьи";
+    private String climbHow = "ловко";
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected String say() {
+        return super.say(saySound);
+    }
+
+    @Override
+    protected String swim() {
+        return super.swim(swimHow);
+    }
+
+    @Override
+    protected String eat() {
+        return super.eat(eatWhat);
+    }
+
+    @Override
+    protected String move() {
+        return super.move(moveHow);
+    }
+
+    @Override
+    protected String climb() {
+        return super.climb(climbHow);
+    }
 
     protected String description() {
-        return "Кошка " + name + ' ' + say("мяу") + ", " + climb("ловко") + ", но " +
-                swim("плохо") + ", " + eat("мышей") + " и " + move("по-кошачьи");
+        return "Кошка " + name + ' ' + say() + ", " + climb() + ", но " +
+                swim() + ", " + eat() + " и " + move();
     }
 }
