6cad874cbed96cf3b8ab1a8ea67aa347642549d5
proweb4all <proweb4all@narod.ru>
2020-08-22 23:38:25 +0300

    update-refactoring

--- src/main/java/ru/sbrf/lesson6/Dog.java
+++ src/main/java/ru/sbrf/lesson6/Dog.java
@@ -1,11 +1,43 @@
 package ru.sbrf.lesson6;
 
 public class Dog {
-    private Animal dog = new Animal();
+    Animal animal = new Animal();
+    private String name = "NoName";
+    private String saySound = "гав";
+    private String swimHow = "хорошо";
+    private String eatWhat = "мясо";
+    private String moveHow = "быстро";
+    private String climbHow = "плохо";
 
-    protected void description(String name) {
-        System.out.print("Собака " + name + ' ' + dog.say("гав") + ", ");
-        System.out.print(dog.climb("плохо") + ", но " + dog.swim("хорошо") + ", ");
-        System.out.println(dog.eat("мясо") + " и " + dog.move("быстро"));
+    public Dog() {
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    String say() {
+        return animal.say(saySound);
+    }
+
+    String swim() {
+        return animal.swim(swimHow);
+    }
+
+    String eat() {
+        return animal.eat(eatWhat);
+    }
+
+    String move() {
+        return animal.move(moveHow);
+    }
+
+    String climb() {
+        return animal.climb(climbHow);
+    }
+
+    String description() {
+        return "Собака " + name + ' ' + say() + ", " + climb() + ", но " +
+                swim() + ", " + eat() + " и " + move();
     }
 }
--- src/main/java/ru/sbrf/lesson6/Main.java
+++ src/main/java/ru/sbrf/lesson6/Main.java
@@ -2,10 +2,36 @@ package ru.sbrf.lesson6;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.name = "Мурка";
+        System.out.println("--- Animal ---");
+        Animal animal = new Animal();
+        System.out.println(animal.say());
+        System.out.println(animal.swim());
+        System.out.println(animal.eat());
+        System.out.println(animal.move());
+        System.out.println(animal.climb());
+
+        System.out.println("--- Cat ---");
+        Cat cat = new Cat("Мурка");
         System.out.println(cat.description());
-        Dog dog = new Dog();
-        dog.description("Жучка");
+        System.out.println(cat.say());
+        System.out.println(cat.swim());
+        System.out.println(cat.eat());
+        System.out.println(cat.move());
+        System.out.println(cat.climb());
+        Cat cat1 = new Cat();
+        System.out.println(cat1.description());
+        System.out.println(cat.eat("кузнечиков"));
+
+        System.out.println("--- Dog ---");
+        Dog dog = new Dog("Жучка");
+        System.out.println(dog.description());
+        System.out.println(dog.say());
+        System.out.println(dog.swim());
+        System.out.println(dog.eat());
+        System.out.println(dog.move());
+        System.out.println(dog.climb());
+        Dog dog1 = new Dog();
+        System.out.println(dog1.description());
+        System.out.println(dog1.animal.eat("кошек"));
     }
 }
