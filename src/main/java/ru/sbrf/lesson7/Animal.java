ab5d320d281cba5a9d9a61bc80e8e0059a12cb05
proweb4all <proweb4all@narod.ru>
2020-09-02 14:53:28 +0300

    updated interface

--- src/main/java/ru/sbrf/lesson7/Animal.java
+++ src/main/java/ru/sbrf/lesson7/Animal.java
@@ -1,17 +1,28 @@
 package ru.sbrf.lesson7;
 
 public interface Animal {
-    //    default String say() { return "говорит (?)"; }//    default String swim() { return "плавает (?)"; }
-//    default String eat() { return "кушает (?)"; }
-//    default String move() { return "двигается (?)"; }
-//    default String climb() { return "лазает по деревьям (?)"; }
-    String say();
+    default String say() {
+        return "говорит (?)";
+    }
 
-    String swim();
+    default String swim() {
+        return "плавает (?)";
+    }
 
-    String eat();
+    default String eat() {
+        return "кушает (?)";
+    }
 
-    String move();
+    default String move() {
+        return "двигается (?)";
+    }
 
-    String climb();
+    default String climb() {
+        return "лазает по деревьям (?)";
+    }
+    //String say();
+    //String swim();
+    //String eat();
+    //String move();
+    //String climb();
 }
